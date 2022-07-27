10b5a956d706197d4389f7387c183ea5017a5446
lenongo <funakki0904@gmail.com>
2022-07-28 08:11:58 +0900

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import List from "./components/list"
+import { useState,useEffect } from 'react'
 
 function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() =>{
+    fetch('https://btcqpan7rg.execute-api.ap-northeast-1.amazonaws.com/dev/translate')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List msg="hello"/>
+      <p>{message}</p>
     </div>
   );
 }
---
+++ src/components/list.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const list = (props) => {
+  return (
+    <div>
+        <p>
+            username:{props.msg}
+        </p>
+        <p>
+            userID
+        </p>
+    </div>
+  )
+}
+
+export default list
